446866dab8aa5ce461a4a5ca682cb4db2ceca7c8
hrayr <lincerta@gmail.com>
2016-12-29 18:04:22 +0400

    st

--- ski-slider.jquery.js
+++ ski-slider.jquery.js
@@ -9,13 +9,35 @@
 			// dots: false,
 			slides: 1,
 			complete: null,
+			arrows: true,
 		}, options);
 
+
 		this.slides = null;
+		this.viewportWidth = $(this).width();
+		this.itemsCount = $(this).length;
+		this.itemWidth = this.viewportWidth / defaults.slides;
+		this.itemsFullWidth = this.itemWidth * this.itemsCount;
 		
 
 		return this.each(function(){
-			$(this).wrap('<div class="ski-slider-wrapper"></div>')
+			$(this)
+				.wrapInner('<div class="ski-slider-items"></div>')
+				.addClass('ski-slider-viewport')
+				.find('.ski-slider-items').width(this.itemsFullWidth);
+
+
+
+			if (defaults.arrows) {
+				$(this)
+					.append('<div class="ski-arrows"></div>')
+					.find('.ski-arrows')
+					.append('<button class="ski-prev">Prev</button>')
+					.append('<button class="ski-next">Next</button>')
+			}
+
+
+
 
 			// complete callback				
 			if ($.isFunction(defaults.complete)) {
